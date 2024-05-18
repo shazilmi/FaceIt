2e0a0d88700387effba4092a9167ae14e71fccc5
Shazil Mohammad Iqbal <shazilmiqbal@protonmail.com>
2024-05-18 08:30:38 +0530

    Requirements.txt

--- user/about.py
+++ user/about.py
@@ -4,7 +4,6 @@ from flask_login import current_user, login_required
 # Creating a blueprint for the user dashboard.
 abouts = Blueprint('about', __name__)
 @abouts.route('/about', methods = ['GET'])
-@login_required
 def about():
 	if request.method == 'GET':
 		return render_template('about.html')
